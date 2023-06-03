e55c396e6eaca17be00d07c7820b390c5dcd84e0
Vishaal L.S <62366204+Vishaal-LS@users.noreply.github.com>
2023-06-03 13:48:28 +0530

    3.13(roots_of_quadratic_eq)

--- cpp/Alpha/Alpha.cpp
+++ cpp/Alpha/Alpha.cpp
@@ -1,41 +1,31 @@
-// Program to enter marks of students in 4 subject
-
+// Program 3.13 Write a program to calculate roots of a quadratic equation.
 #include<iostream>
+#include<cmath>
 using namespace std;
-
 int main()
 {
-    int marks1, marks2, marks3, marks4, total = 0;
-    float avg = 0.0;
-    
-    cout << "Enter the marks in Mathematics, Science, Social Science, and Computers: ";
-    cin >> marks1 >> marks2 >> marks3 >> marks4;
-    
-    total = marks1 + marks2 + marks3 + marks4;
-    avg = static_cast<float>(total) / 4;
-    
-    cout << "Total: " << total << ", Average: " << avg << endl;
-    
-    if (avg >= 75)
-    {
-        cout << "Distinction" << endl;
-    }
-    else if (avg >= 60)
-    {
-        cout << "First Division" << endl;
-    }
-    else if (avg >= 50)
-    {
-        cout << "Second Division" << endl;
-    }
-    else if (avg >= 40)
-    {
-        cout << "Third Division" << endl;
-    }
-    else
-    {
-        cout << "FAIL" << endl;
-    }
-
-    return 0;
+	int a, b, c;
+	float D, deno, root1, root2;
+	cout << "\n Enter the values of a, b, c : ";
+	cin >> a >> b >> c;
+	D = (b * b) - (4 * a * c);
+	deno = 2 * a;
+	if (D > 0)
+	{
+		cout << "\n REAL ROOTS";
+		root1 = (-b + sqrt(D)) / deno;
+		root2 = (-b - sqrt(D)) / deno;
+		cout << "\n ROOT1 = " << root1 << "\t ROOT2 = " << root2;
+	}
+	else if (D == 0)
+	{
+		cout << "\n EQUAL ROOTS";
+		root1 = -b / deno;
+		cout << "\n ROOT1 = " << root1;
+	}
+	else
+	{
+		cout << "\n IMAGINARY ROOTS";
+	}
+	return 0;
 }
