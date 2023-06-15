1a58bf9c72cbec27a0552a25b205ca88e70b9038
Vishaal L.S <62366204+Vishaal-LS@users.noreply.github.com>
2023-06-16 05:27:52 +0530

    largestof_n_array

--- cpp/Alpha/Alpha.cpp
+++ cpp/Alpha/Alpha.cpp
@@ -1,30 +1,47 @@
-// Program to read and display n numbers using an array
+// prorgam to find largest of n numbers using arrays
 
 #include <iostream>
-#include <string>
+#include<stdlib.h>
 
-using namespace std;
 
 int main()
 {
+	int arr[50], i, large, n;
 	
-	int i = 0, n, arr[20];//declaration of variables and array
+	std::cout << "Enter the number of elements in the array :- \n";
+	std::cin >> n;
 	
-	cout << "\n Enter the number of elements of array : ";
-	cin >> n;
+	std::cout << "Enter the elements of Array : \n";
 
 	for (i = 0; i < n; i++)
 	{
-		cout << "\n Arr[" << i << "]";
-		cin >> arr[i];
+		std::cin >> arr[i];
 	}
-
-	cout << "\n The array elements are : \n";
-	
-	for (i = 0; i < n; i++)
+	large = arr[0];
+	for (i = 1; i < n; i++)
 	{
-		cout << "\t Arr[" << i << "]  = " << arr[i] <<"\n";
+		if (arr[i] > large)
+			large = arr[i]	
 	}
-
+	std::cout << "\n The Largest number is : " << large;
 }
 
+/*
+1. Delcare an array "arr" of size 50.
+
+2. Declare variables 'i', 'large', 'n', which represent loop counter, largest number and the number of elements respectively.
+
+3. print "Enter the number of elements in the array : ".
+
+4. read values of 'n' from the user.
+
+5. print "Enter the elements of Array : ".
+
+6. start a forloop from 0 to n-1 as long as i is smaller than n, i gets incremented and value of 'i' is read for the array elements.
+
+7. set 'large' to first element of array.
+
+8. start a forloop from 1 to n-1 as long as i < n, i gets incremented, check if 'arr[i]' is greater than 'large', assign large to arr[i].
+
+9. Print "The largest number is : " into largest number.
+*/
