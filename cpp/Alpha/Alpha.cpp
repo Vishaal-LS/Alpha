5140d3a86682f35ef08e6a8f56138b0d444b12cc
Vishaal L.S <62366204+Vishaal-LS@users.noreply.github.com>
2023-06-15 10:07:37 +0530

    read&display_n_usingarray

--- cpp/Alpha/Alpha.cpp
+++ cpp/Alpha/Alpha.cpp
@@ -1,3 +1,5 @@
+// Program to read and display n numbers using an array
+
 #include <iostream>
 #include <string>
 
@@ -5,7 +7,24 @@ using namespace std;
 
 int main()
 {
-	int num1, num2;
-	cout << "Enter the first number and second number: ";
-	cin >> num1 >> num2;
+	
+	int i = 0, n, arr[20];//declaration of variables and array
+	
+	cout << "\n Enter the number of elements of array : ";
+	cin >> n;
+
+	for (i = 0; i < n; i++)
+	{
+		cout << "\n Arr[" << i << "]";
+		cin >> arr[i];
+	}
+
+	cout << "\n The array elements are : \n";
+	
+	for (i = 0; i < n; i++)
+	{
+		cout << "\t Arr[" << i << "]  = " << arr[i] <<"\n";
+	}
+
 }
+
