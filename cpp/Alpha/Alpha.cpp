a644f6b62c3e19d35445e31c08aade63d49cac31
Vishaal L.S <62366204+Vishaal-LS@users.noreply.github.com>
2023-06-16 17:39:32 +0530

    insert element in array.cpp

--- cpp/Alpha/Alpha.cpp
+++ cpp/Alpha/Alpha.cpp
@@ -1,90 +1,61 @@
-// Program to interchange large and small number in array
+//WAP to insert number in given location of array
+/*
+PesudoCode: 
 
-#include<iostream>
+1. declare i, n, num, pos, arr[10] as int
+
+2. print "\n Enter the number of elements in the array : \n" and read n
+
+3. print "\n Enter the elements : \n" and start forloop where i = 0 , ALA i is less than n, increment i.
+	a)read arr[i]
+
+4. print "\n Enter the number to be inserted : "
+	a)read num
+
+5. print \n Enter the position at which the number has to be added : "
+	a)read pos
+
+6. forloop where i = n, ALA i greater or equal to pos, decrement i
+	a)arr[i+1] = arr[i]
+	b)arr[pos] = num;
+
+7. print "\n The array after insertion is" num "is : "
+	a)initialize forloop where i = 0, ALA i smaller than n+1, increment i
+	b)print "\n Arr[:"<< i << "] = " << arr[i]"
+
+*/
+#include <iostream>
 
 int main()
 {
-    int i, n, arr[20], temp;
-    int small, small_pos = 0, large, large_pos = 0;
+    int i, n, num, pos, arr[10];
 
-    std::cout << "********Program to interchange large and small number in array************\n\n";
-
-    std::cout << "Enter the number of elements in the array: ";
+    std::cout << "\n Enter the number of elements in the array: ";
     std::cin >> n;
 
-    std::cout << "Enter the elements of the array: ";
+    std::cout << "\n Enter the elements: ";
     for (i = 0; i < n; i++)
     {
         std::cin >> arr[i];
     }
 
-    small = large = arr[0];
-    for (i = 0; i < n; i++)
+    std::cout << "Enter the number to be inserted: ";
+    std::cin >> num;
+
+    std::cout << "Enter the position at which the number has to be added: ";
+    std::cin >> pos;
+
+    for (i = n; i >= pos; i--)
     {
-        if (arr[i] < small)
-        {
-            small = arr[i];
-            small_pos = i;
-        }
-        if (arr[i] > large)
-        {
-            large = arr[i];
-            large_pos = i;
-        }
+        arr[i + 1] = arr[i];
     }
-    std::cout << "The smallest number is: " << small << "\n";
-    std::cout << "The position of the smallest number in the array is: " << small_pos + 1 << "\n";
+    arr[pos] = num;
 
-    std::cout << "The largest number is: " << large << "\n";
-    std::cout << "The position of the largest number in the array is: " << large_pos + 1 << "\n";
-
-    temp = arr[large_pos];
-    arr[large_pos] = arr[small_pos];
-    arr[small_pos] = temp;
-
-    std::cout << "\nThe new array is: ";
-    for (i = 0; i < n; i++)
+    std::cout << "The array after inserting " << num << " is: ";
+    for (i = 0; i < n + 1; i++)
     {
-        std::cout << arr[i] << " ";
+        std::cout << "Arr[" << i << "] = " << arr[i] << " ";
     }
-    std::cout << "\n\n****************************************************\n";
+
+    return 0;
 }
-
-
-
-/*
-Pesudocode:
-
-1. Variable Declaration of i , n, arr[20], temp, small, small_pos, large, large_pos.
-
-2. print "Enter the number of elements in the array : " and read value of n.
-
-3. print "Enter the elements of the array : ", 
-	a)initiate a forloop where i is 0 and ALA i < n , i gets incremented.
-	b)read value of arr[i]
-
-4. declare small = large = arr[0].
-
-5. initiate forloop for i where i =0, and if i is smaller than  n , i gets incremented
-	a) if arr[i] is smaller than "small", small = arr[i] and small_pos = 1.
-	b) if arr[i] is larger than "large", large = arr[i] and large_pos = 1.
-====
-6. print "The smallest number of these numbers is : " 
-	a) "small".
-
-7. print "The position of the smallest number in the array is : ".
-	a) "small_pos".
-
-8. print "The largest of these numbers is : " 
-	a) "large"
-
-9. print "The position of the largest number in the array is : "
-	a) large_pos
-======
-10. assign temp to arr[large_pos] and make arr[large_pos] = arr[small_pos] and assign arr[small_pos] to temp
-
-11. print "The new array is : "
-	a)forloop where i = 0 and as long as i is less than n, i is incremented.
-	b)print arr[i]
-
-*/
