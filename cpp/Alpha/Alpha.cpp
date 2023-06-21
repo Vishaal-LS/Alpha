70f19aaa1136188e65d567e4a6c1cef6f9987510
Vishaal L.S <62366204+Vishaal-LS@users.noreply.github.com>
2023-06-21 19:53:02 +0530

    Recursion.cpp

--- cpp/Alpha/Alpha.cpp
+++ cpp/Alpha/Alpha.cpp
@@ -1,16 +1,20 @@
-//program to add two floating point numbers 
+// Recursive program
 #include <iostream>
+
+void myFunction(int myNumbers[5])
+
+{
+	using namespace std;
+	
+	for (int i = 0; i < 5; i++) 
+	{
+		cout << myNumbers[i] << "\n";
+	}
+}
 
 int main()
 {
-	using namespace std;
-
-	float num1, num2, sum = 0.0;
-	float* pnum1 = &num1, * pnum2 = &num2, * psum = &sum;
-	cout << "\n Enter the two numbers : ";
-	cin >> * pnum1 >> * pnum2;
-
-	*psum = *pnum1 + *pnum2;
-	  
-	cout << "\n" << *pnum1 << " + " << *pnum2 << " = " << *psum;
+	int myNumbers[5] = { 10, 20, 30, 40, 50 };
+	myFunction(myNumbers);
+	return 0;
 }
